5dfb9c71be5cab546e013bd1efdbcf22e838198e
lovepreet kaur <preetlove73@gmail.com>
2018-10-17 19:31:02 +1300

    Add project files.

---
+++ MVCdemo1/Controllers/employeeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCdemo1.Models;
+
+namespace MVCdemo1.Controllers
+{
+    public class employeeController : Controller
+    {
+        // GET: employee
+        public ActionResult View()
+        {
+			var Employee = new employee() { Name = "gary" };
+				return View(Employee);
+        }
+    }
+}
---
+++ MVCdemo1/Models/employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCdemo1.Models
+{
+	public class employee
+	{
+		public int id { get; set; }
+		public String Name { get; set; }
+		
+	}
+}
